955bcecdc25d0e4005b8153b5a6150d4e9d9e3f9
Alfian Hamdani <alfian.hamdani04@gmail.com>
2021-01-25 20:44:53 +0800

    Add detail responses

--- app/src/main/java/com/dicoding/picodiploma/movieapplication/api/ApiService.kt
+++ app/src/main/java/com/dicoding/picodiploma/movieapplication/api/ApiService.kt
@@ -4,16 +4,29 @@ import com.dicoding.picodiploma.movieapplication.data.MovieResponse
 import com.dicoding.picodiploma.movieapplication.data.TVSeriesResponse
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface ApiService {
     @GET("movie/upcoming")
     fun getMovie(
-        @Query("api_key") apiKey: String
+            @Query("api_key") apiKey: String
     ): Call<MovieResponse>
 
     @GET("tv/top_rated")
     fun getTVSeries(
-        @Query("api_key") apiKey: String
+            @Query("api_key") apiKey: String
     ): Call<TVSeriesResponse>
+
+    @GET("movie/{movie_id}")
+    fun getDetailMovie(
+            @Path("movie_id") movieId: Int,
+            @Query("api_key") apiKey: String
+    )
+
+    @GET("tv/{tv_id}")
+    fun getDetailTVSeries(
+            @Path("tv_id") tvSeriesId: Int,
+            @Query("api_key") apiKey: String
+    )
 }
--- app/src/main/java/com/dicoding/picodiploma/movieapplication/ui/detail/DetailViewModel.kt
+++ app/src/main/java/com/dicoding/picodiploma/movieapplication/ui/detail/DetailViewModel.kt
@@ -1,20 +1,39 @@
 package com.dicoding.picodiploma.movieapplication.ui.detail
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.dicoding.picodiploma.movieapplication.BuildConfig
+import com.dicoding.picodiploma.movieapplication.data.DetailMovieResponse
+import com.dicoding.picodiploma.movieapplication.data.DetailTVSeriesResponse
 import com.dicoding.picodiploma.movieapplication.data.MovieEntity
 import com.dicoding.picodiploma.movieapplication.data.TVSeriesEntity
 import com.dicoding.picodiploma.movieapplication.utils.DataDummy
 
 class DetailViewModel: ViewModel() {
-    private var movieId: Int = 0
-    private var tvSeriesId: Int = 0
+    private var movieIdTest: Int = 0
+    private var tvSeriesIdTest: Int = 0
+
+    private val _movieId = MutableLiveData<DetailMovieResponse>()
+    val movieId: LiveData<DetailMovieResponse> = _movieId
+
+    private val _tvSeriesId = MutableLiveData<DetailTVSeriesResponse>()
+    val tvSeriesId: LiveData<DetailTVSeriesResponse> = _tvSeriesId
+
+    private val _isLoading = MutableLiveData<Boolean>()
+    val isLoading: LiveData<Boolean> = _isLoading
+
+    companion object{
+        private const val TAG = "DetailViewModel"
+        private const val API_KEY = BuildConfig.TMBDApiKey
+    }
 
     fun setSelectedMovie(movieId: Int){
-        this.movieId = movieId
+        this.movieIdTest = movieId
     }
 
     fun setSelectedTVSeries(tvSeriesId: Int){
-        this.tvSeriesId = tvSeriesId
+        this.tvSeriesIdTest = tvSeriesId
     }
 
     fun getMovie(): MovieEntity{
@@ -22,7 +41,7 @@ class DetailViewModel: ViewModel() {
         val movieEntities = DataDummy.generateDummyMovies()
 
         for(movieEntity in movieEntities){
-            if(movieEntity.movieId == movieId){
+            if(movieEntity.movieId == movieIdTest){
                 movie = movieEntity
             }
         }
@@ -35,7 +54,7 @@ class DetailViewModel: ViewModel() {
         val tvSeriesEntities = DataDummy.generateDummyTVSeries()
 
         for(tvSeriesEntity in tvSeriesEntities){
-            if(tvSeriesEntity.tvSeriesId == tvSeriesId){
+            if(tvSeriesEntity.tvSeriesId == tvSeriesIdTest){
                 tvSeries = tvSeriesEntity
             }
         }
