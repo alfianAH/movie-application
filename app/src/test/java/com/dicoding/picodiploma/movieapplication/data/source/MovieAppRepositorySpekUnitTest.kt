61391a5b57283bee9a73126ee77c8eb939954848
Alfian Hamdani <alfian.hamdani04@gmail.com>
2021-02-20 21:01:18 +0800

    Spek Unit Testing on Movie App Repository

--- app/src/test/java/com/dicoding/picodiploma/movieapplication/data/source/MovieAppRepositorySpekUnitTest.kt
+++ app/src/test/java/com/dicoding/picodiploma/movieapplication/data/source/MovieAppRepositorySpekUnitTest.kt
@@ -2,43 +2,44 @@ package com.dicoding.picodiploma.movieapplication.data.source
 
 import androidx.arch.core.executor.ArchTaskExecutor
 import androidx.arch.core.executor.TaskExecutor
-import com.dicoding.picodiploma.movieapplication.BuildConfig
+import androidx.lifecycle.MutableLiveData
+import androidx.paging.DataSource
+import com.dicoding.picodiploma.movieapplication.data.source.local.LocalDataSource
+import com.dicoding.picodiploma.movieapplication.data.source.local.entity.movie.MovieDetails
+import com.dicoding.picodiploma.movieapplication.data.source.local.entity.movie.MovieEntity
+import com.dicoding.picodiploma.movieapplication.data.source.local.entity.movie.MovieGenreEntity
+import com.dicoding.picodiploma.movieapplication.data.source.local.entity.tvseries.TVSeriesDetails
+import com.dicoding.picodiploma.movieapplication.data.source.local.entity.tvseries.TVSeriesEntity
+import com.dicoding.picodiploma.movieapplication.data.source.local.entity.tvseries.TVSeriesGenreEntity
 import com.dicoding.picodiploma.movieapplication.data.source.remote.RemoteDataSource
-import com.dicoding.picodiploma.movieapplication.data.source.remote.response.DetailMovieResponse
-import com.dicoding.picodiploma.movieapplication.data.source.remote.response.DetailTVSeriesResponse
-import com.dicoding.picodiploma.movieapplication.data.source.remote.response.MovieResultsItem
-import com.dicoding.picodiploma.movieapplication.data.source.remote.response.TVSeriesResultsItem
-import com.dicoding.picodiploma.movieapplication.utils.APILoaderHelper
-import com.dicoding.picodiploma.movieapplication.utils.LiveDataTestUtil
-import com.nhaarman.mockitokotlin2.any
-import com.nhaarman.mockitokotlin2.doAnswer
-import com.nhaarman.mockitokotlin2.eq
+import com.dicoding.picodiploma.movieapplication.utils.*
+import com.dicoding.picodiploma.movieapplication.valueobject.Resource
 import com.nhaarman.mockitokotlin2.verify
 import org.junit.Assert.*
 import org.junit.platform.runner.JUnitPlatform
 import org.junit.runner.RunWith
 import org.mockito.Mockito
+import org.mockito.Mockito.mock
 import org.spekframework.spek2.Spek
-import retrofit2.Call
 
 @RunWith(JUnitPlatform::class)
 class MovieAppRepositorySpekUnitTest : Spek({
+    val remote = mock(RemoteDataSource::class.java)
+    val localDataSource = mock(LocalDataSource::class.java)
+    val appExecutors = mock(AppExecutors::class.java)
 
-    lateinit var detailMovieResponse: Call<DetailMovieResponse>
-    lateinit var detailTVSeriesResponse: Call<DetailTVSeriesResponse>
-    lateinit var movieEntities: List<MovieResultsItem>
-    lateinit var tvSeriesEntities: List<TVSeriesResultsItem>
-    var movieId: Int? = 0
-    var tvSeriesId: Int? = 0
+    val movieAppRepository = FakeMovieAppRepository(remote, localDataSource, appExecutors)
 
-    val apiKey = BuildConfig.TMBDApiKey
-    val movieResponse = APILoaderHelper().findMovies(apiKey)
-    val tvSeriesResponse = APILoaderHelper().findTVSeries(apiKey)
-    val remote = Mockito.mock(RemoteDataSource::class.java)
-    val movieAppRepository = FakeMovieAppRepository(remote)
+    val movieResponses = DataDummy.generateRemoteDummyMovies()
+    val movieId = movieResponses[0].id
+    val movieGenresResponse = DataDummy.generateRemoteDummyMovieGenres(movieId)
+    val tvSeriesResponses = DataDummy.generateRemoteDummyTVSeries()
+    val tvSeriesId = tvSeriesResponses[0].id
+    val tvSeriesGenresResponse = DataDummy.generateRemoteDummyTVSeriesGenres(movieId)
 
     group("Movie App Repository Test"){
         beforeEachTest {
+            // Instant Task Executor Rule substitute in spek unit testing
             ArchTaskExecutor.getInstance().setDelegate(object: TaskExecutor() {
                 override fun executeOnDiskIO(runnable: Runnable) {
                     runnable.run()
@@ -56,80 +57,120 @@ class MovieAppRepositorySpekUnitTest : Spek({
             ArchTaskExecutor.getInstance().setDelegate(null)
         }
 
-        group("Get Movie"){
-            beforeEachTest {
-                doAnswer { invocation ->
-                    (invocation.arguments[1] as RemoteDataSource.LoadMoviesCallback)
-                            .onAllMoviesReceived(movieResponse)
-                    null
-                }.`when`(remote).getMovies(eq(apiKey), any())
+        test("Get Movie"){
+            val dataSourceFactory = mock(DataSource.Factory::class.java) as DataSource.Factory<Int, MovieEntity>
 
-                movieEntities = LiveDataTestUtil.getValue(movieAppRepository.getMovies(apiKey))
+            Mockito.`when`(localDataSource.getMovies(SortUtils.NAME)).thenReturn(dataSourceFactory)
+            movieAppRepository.getMovies(SortUtils.NAME)
 
-                movieId = movieEntities[0].id
-                detailMovieResponse = APILoaderHelper().findDetailMovie(apiKey, movieId as Int)
-            }
+            val movieEntities = Resource.success(PagedListUtil.mockPagedList(DataDummy.generateDummyMovies()))
 
-            test("Get movie"){
-                verify(remote).getMovies(eq(apiKey), any())
-                assertNotNull(movieEntities)
-                assertEquals(20, movieEntities.size)
-            }
+            verify(localDataSource).getMovies(SortUtils.NAME)
+            assertNotNull(movieEntities.data)
+            assertEquals(movieResponses.size, movieEntities.data?.size)
         }
 
-        group("Get TV Series"){
-            beforeEachTest {
-                doAnswer { invocation ->
-                    (invocation.arguments[1] as RemoteDataSource.LoadTVSeriesCallback)
-                            .onAllTVSeriesReceived(tvSeriesResponse)
-                    null
-                }.`when`(remote).getTVSeries(eq(apiKey), any())
+        test("Get TV Series"){
+            val dataSourceFactory = mock(DataSource.Factory::class.java) as DataSource.Factory<Int, TVSeriesEntity>
+            Mockito.`when`(localDataSource.getTVSeries(SortUtils.NAME)).thenReturn(dataSourceFactory)
+            movieAppRepository.getTVSeries(SortUtils.NAME)
 
-                tvSeriesEntities = LiveDataTestUtil.getValue(movieAppRepository.getTVSeries(apiKey))
+            val tvSeriesEntities = Resource.success(PagedListUtil.mockPagedList(DataDummy.generateDummyTVSeries()))
 
-                tvSeriesId = tvSeriesEntities[0].id
-                detailTVSeriesResponse = APILoaderHelper().findDetailTVSeries(apiKey, tvSeriesId as Int)
-            }
-
-            test("Get TV Series"){
-                verify(remote).getTVSeries(eq(apiKey), any())
-                assertNotNull(tvSeriesEntities)
-                assertEquals(20, tvSeriesEntities.size)
-            }
+            verify(localDataSource).getTVSeries(SortUtils.NAME)
+            assertNotNull(tvSeriesEntities.data)
+            assertEquals(tvSeriesResponses.size, tvSeriesEntities.data?.size)
         }
 
-        group("Get Detail Movie"){
-            test("Get Detail Movie"){
-                doAnswer { invocation ->
-                    (invocation.arguments[2] as RemoteDataSource.LoadDetailMovieCallback)
-                            .onDetailMovieReceived(detailMovieResponse)
-                    null
-                }.`when`(remote).getDetailMovie(eq(apiKey), eq(movieId as Int), any())
+        test("Get Detail Movie"){
+            val dummyMovie = MutableLiveData<MovieDetails>()
+            dummyMovie.value = DataDummy.generateDummyDetailMovie(DataDummy.generateDummyMovies()[0], false)
 
-                val detailMovieEntity =
-                        LiveDataTestUtil.getValue(movieAppRepository.getDetailMovie(apiKey, movieId as Int))
+            Mockito.`when`(localDataSource.getDetailMovieById(movieId)).thenReturn(dummyMovie)
 
-                verify(remote).getDetailMovie(eq(apiKey), eq(movieId as Int), any())
-                assertNotNull(detailMovieEntity)
-                assertEquals("Wonder Woman 1984", detailMovieEntity.title)
-            }
+            val movieEntity = LiveDataTestUtil.getValue(movieAppRepository.getDetailMovie(movieId)).data
+
+            verify(localDataSource).getDetailMovieById(movieId)
+            assertNotNull(movieEntity)
+            assertNotNull(movieEntity?.movieEntity?.title)
+            assertEquals(movieResponses[0].title, movieEntity?.movieEntity?.title)
+            assertEquals(movieResponses[0].overview, movieEntity?.movieEntity?.overview)
+            assertEquals(movieResponses[0].releaseDate, movieEntity?.movieEntity?.releaseDate)
+            assertEquals(movieResponses[0].id, movieEntity?.movieEntity?.id)
+            assertEquals(movieResponses[0].posterPath, movieEntity?.movieEntity?.posterPath)
+            assertEquals(movieResponses[0].voteAverage, movieEntity?.movieEntity?.voteAverage)
         }
 
-        group("Get Detail TV Series"){
-            test("Get Detail TV Series"){
-                doAnswer { invocation ->
-                    (invocation.arguments[2] as RemoteDataSource.LoadDetailTVSeriesCallback)
-                            .onDetailTVSeriesReceived(detailTVSeriesResponse)
-                    null
-                }.`when`(remote).getDetailTVSeries(eq(apiKey), eq(tvSeriesId as Int), any())
 
-                val detailTVSeriesEntity =
-                        LiveDataTestUtil.getValue(movieAppRepository.getDetailTVSeries(apiKey, tvSeriesId as Int))
+        test("Get Detail TV Series"){
+            val dummyTVSeries = MutableLiveData<TVSeriesDetails>()
+            dummyTVSeries.value = DataDummy.generateDummyDetailTVSeries(DataDummy.generateDummyTVSeries()[0], false)
 
-                verify(remote).getDetailTVSeries(eq(apiKey), eq(tvSeriesId as Int), any())
-                assertNotNull(detailTVSeriesEntity)
-                assertEquals("I Am Not an Animal", detailTVSeriesEntity.name)
-            }
+            Mockito.`when`(localDataSource.getDetailTVSeriesById(tvSeriesId)).thenReturn(dummyTVSeries)
+
+            val tvSeriesEntity = LiveDataTestUtil.getValue(movieAppRepository.getDetailTVSeries(tvSeriesId)).data
+
+            verify(localDataSource).getDetailTVSeriesById(tvSeriesId)
+            assertNotNull(tvSeriesEntity)
+            assertNotNull(tvSeriesEntity?.tvSeriesEntity?.title)
+            assertEquals(tvSeriesResponses[0].name, tvSeriesEntity?.tvSeriesEntity?.title)
+            assertEquals(tvSeriesResponses[0].overview, tvSeriesEntity?.tvSeriesEntity?.overview)
+            assertEquals(tvSeriesResponses[0].firstAirDate, tvSeriesEntity?.tvSeriesEntity?.releaseDate)
+            assertEquals(tvSeriesResponses[0].id, tvSeriesEntity?.tvSeriesEntity?.id)
+            assertEquals(tvSeriesResponses[0].posterPath, tvSeriesEntity?.tvSeriesEntity?.posterPath)
+            assertEquals(tvSeriesResponses[0].voteAverage, tvSeriesEntity?.tvSeriesEntity?.voteAverage)
+        }
+
+        test("Get Movie Genres"){
+            val dummyGenres = MutableLiveData<List<MovieGenreEntity>>()
+            dummyGenres.value = DataDummy.generateDummyMovieGenres(movieId)
+
+            Mockito.`when`(localDataSource.getMovieGenresById(movieId)).thenReturn(dummyGenres)
+
+            val genreEntities = LiveDataTestUtil.getValue(movieAppRepository.getMovieGenres(movieId)).data
+
+            verify(localDataSource).getMovieGenresById(movieId)
+            assertNotNull(genreEntities)
+            assertEquals(movieGenresResponse.size, genreEntities?.size)
+        }
+
+        test("Get TV Series Genres"){
+            val dummyGenres = MutableLiveData<List<TVSeriesGenreEntity>>()
+            dummyGenres.value = DataDummy.generateDummyTVSeriesGenres(tvSeriesId)
+
+            Mockito.`when`(localDataSource.getTVSeriesGenresById(tvSeriesId)).thenReturn(dummyGenres)
+
+            val genreEntities = LiveDataTestUtil.getValue(movieAppRepository.getTVSeriesGenres(tvSeriesId)).data
+
+            verify(localDataSource).getTVSeriesGenresById(tvSeriesId)
+            assertNotNull(genreEntities)
+            assertEquals(tvSeriesGenresResponse.size, genreEntities?.size)
+        }
+
+        test("Get Favorite Movies"){
+            val dataSourceFactory = mock(DataSource.Factory::class.java) as DataSource.Factory<Int, MovieEntity>
+
+            Mockito.`when`(localDataSource.getFavoriteMovies(SortUtils.NAME)).thenReturn(dataSourceFactory)
+            movieAppRepository.getFavoriteMovies(SortUtils.NAME)
+
+            val movieEntities = Resource.success(PagedListUtil.mockPagedList(DataDummy.generateDummyMovies()))
+
+            verify(localDataSource).getFavoriteMovies(SortUtils.NAME)
+            assertNotNull(movieEntities.data)
+            assertEquals(movieResponses.size, movieEntities.data?.size)
+        }
+
+        test("Get Favorite TV Series"){
+            val dataSourceFactory = mock(DataSource.Factory::class.java) as DataSource.Factory<Int, TVSeriesEntity>
+
+            Mockito.`when`(localDataSource.getFavoriteTVSeries(SortUtils.NAME)).thenReturn(dataSourceFactory)
+            movieAppRepository.getFavoriteTVSeries(SortUtils.NAME)
+
+            val tvSeriesEntity = Resource.success(PagedListUtil.mockPagedList(DataDummy.generateDummyTVSeries()))
+
+            verify(localDataSource).getFavoriteTVSeries(SortUtils.NAME)
+            assertNotNull(tvSeriesEntity.data)
+            assertEquals(movieResponses.size, tvSeriesEntity.data?.size)
         }
     }
 })
